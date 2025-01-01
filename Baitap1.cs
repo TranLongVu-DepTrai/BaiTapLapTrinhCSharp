2dd9e250f120bf8729c145a5e12f37316e52780b
TranLongVu-DepTrai <vutl.22it@vku.udn.vn>
2025-01-01 14:08:08 +0700

    Add files via upload

---
+++ Baitap1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyFirstProject
+{
+    class baitap1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("---------CHuong Trinh Tinh 2 So-----------");
+            Console.Write("Nhap so nguyen a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so nguyen b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap Phep Tinh(+,-,*,/): ");
+            char pheptinh = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            double ketqua = 0;
+
+            switch (pheptinh) {
+                case '+': ketqua = a + b;
+                    break;
+                case '-':
+                    ketqua = a - b;
+                    break;
+                case '*':
+                    ketqua = a * b;
+                    break;
+                case '/':
+                    if (b != 0) {
+                        ketqua = (double)a / b;
+                        
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Khong ton tai phep chia 0");
+                        return;
+                    }
+                    break;
+
+                default: Console.WriteLine("Phep tinh khong hop le");
+                    return;
+                    
+
+            }
+            Console.WriteLine($"Ket Qua: {ketqua}");
+        }
+    }
+}
